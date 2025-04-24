79e75de82780fe6d9eb35207e52aa29a7f741701
Exequiel Acosta <exeacostadavila@hotmail.com>
2025-04-24 10:10:53 -0300

    crud tarjetas sin terminar

--- src/components/crudTarjetas/tarjetaModule.js
+++ src/components/crudTarjetas/tarjetaModule.js
@@ -1,12 +1,22 @@
 'use strict';
 
-const { getAllService } = require('./tarjetaService');
+const {  
+	getAllService,
+	getIdService,
+	createCardService,
+	updateCardService,
+	deleteCardService 
+} = require('./tarjetaService');
 const ModuleError = require('../../utils/moduleError');
 const logger = require('../../utils/LoggerCNS').loggerCNS;
 const { INTERNAL_ERROR } = require('../../utils/constantes');
 
+
+
+//module getAllModule
 const getAllModule = async () => {
-	showLogsBanner();
+
+	showBanner('     Iniciando tarjetaModule -> getAll()     ');
 
 	try {
 		const response = await getAllService();
@@ -21,12 +31,92 @@ const getAllModule = async () => {
 	}
 };
 
-module.exports = {
-	getAllModule,
+//module getIdModule
+const getIdModule = async () => {
+
+	showBanner('      Iniciando tarjetaModule -> getId()     ');
+
+	try {
+		const response = await getIdService();
+		return  response;
+	} catch (error) {
+		if (error.statusCode) throw error; // error controlado
+		const moduleError = new ModuleError(error);
+		moduleError.statusCode = INTERNAL_ERROR;
+		logger.error(' Error en tarjetaModule -> getId():');
+		logger.error(' Error module: ', error);
+		throw moduleError;
+	}
 };
 
-function showLogsBanner() {
+//module createCardModule
+const createCardModule = async () => {
+
+	showBanner('   Iniciando tarjetaModule -> createCard()   ');
+
+	try {
+		const response = await createCardService();
+		return response;
+	} catch (error) {
+		if (error.statusCode) throw error; // error controlado
+		const moduleError = new ModuleError(error);
+		moduleError.statusCode = INTERNAL_ERROR;
+		logger.error(' Error en tarjetaModule -> createCard():');
+		logger.error(' Error module: ', error);
+		throw moduleError;
+	}
+};
+
+//module updateCardModule
+const updateCardModule = async () => {
+
+	showBanner('   Iniciando tarjetaModule -> updateCard()   ');
+
+	try {
+		const response = await updateCardService();
+		return response;
+	} catch (error) {
+		if (error.statusCode) throw error; // error controlado
+		const moduleError = new ModuleError(error);
+		moduleError.statusCode = INTERNAL_ERROR;
+		logger.error(' Error en tarjetaModule -> updateCard():');
+		logger.error(' Error module: ', error);
+		throw moduleError;
+	}
+};
+
+//module deleteCardModule
+const deleteCardModule = async () => {
+
+	showBanner('   Iniciando tarjetaModule -> deleteCard()   ');
+
+	try {
+		const response = await deleteCardService();
+		return response;
+	} catch (error) {
+		if (error.statusCode) throw error; // error controlado
+		const moduleError = new ModuleError(error);
+		moduleError.statusCode = INTERNAL_ERROR;
+		logger.error(' Error en tarjetaModule -> deleteCard():');
+		logger.error(' Error module: ', error);
+		throw moduleError;
+	}
+};
+
+//sowBanner
+function showBanner(messageBanner) {
+	
 	logger.info('=============================================');
-	logger.info('     Iniciando tarjetaModule -> getAll()     ');
+	logger.info(messageBanner);
 	logger.info('=============================================');
 }
+
+
+module.exports = {
+	getAllModule,getIdModule,createCardModule,updateCardModule,deleteCardModule
+};
+
+
+
+
+
--- src/components/crudTarjetas/tarjetaService.js
+++ src/components/crudTarjetas/tarjetaService.js
@@ -3,8 +3,9 @@
 const logger = require('../../utils/LoggerCNS').loggerCNS;
 const { executeQuery } = require('../../utils/handleDBPostgres');
 
+//service GetAll
 const getAllService = async () => {
-	showLogsBannerGetAll();
+	showLogsBanner('  Iniciando tarjetaService -> getAll()   ');
 
 	try {
 		const query = `SELECT * FROM "bancoDB".cards ORDER BY balance`; 
@@ -17,16 +18,91 @@ const getAllService = async () => {
 	}
 
 };
+//service getIdService
+const getIdService = async (id) => {
+	showLogsBanner('   Iniciando tarjetaService -> getId()   ');
+
+	try {
+		const query = `SELECT * FROM "bancoDB".cards WHERE id = $1;`; 
+
+		return await executeQuery(query, [id]);
+
+	} catch (error) {
+		logger.error(' Error en tarjetaService -> getId(): ', error);
+		throw error;
+	}
+
+};
+//service crateCardService
+const createCardService = async (body) => {
+	showLogsBanner('Iniciando tarjetaService ->  createCard()');
+
+	try {
+		const query = `INSERT INTO "bancoDB"."cards" (card_number,pin,balance,user_id, createdat) 
+		         VALUES ($1, $2, $3, $4, now()) RETURNING *;`; 
+
+		return await executeQuery(query, [
+			body.card_number,
+			body.pin,
+			body.balance,
+			body.user_id
+
+		]);
+
+	} catch (error) {
+		logger.error(' Error en tarjetaService -> createCard(): ', error);
+		throw error;
+	}
+
+};
+//service updateCardService
+const updateCardService = async (body,id) => {
+	showLogsBanner('Iniciando tarjetaService ->  updateCard()');
+
+	try {
+		const query = `UPDATE "bancoDB"."cards" 
+		         SET card_number=$1,pin=$2,balance=$3,user_id=$4, updatedat = now() 
+		         WHERE id = $5 RETURNING *;`;
+
+		return await executeQuery(query, [
+			body.card_number,
+			body.pin,
+			body.balance,
+			body.user_id,
+			id
+		]);
+
+	} catch (error) {
+		logger.error(' Error en tarjetaService -> updateCard(): ', error);
+		throw error;
+	}
+
+};
+//service updateCardService
+const deleteCardService = async (id) => {
+	showLogsBanner('Iniciando tarjetaService ->  deleteCard()');
+
+	try {
+		const query = `DELETE FROM "bancoDB".cards WHERE id = $1 RETURNING *;`;  
+
+		return await executeQuery(query, [id]);
+
+	} catch (error) {
+		logger.error(' Error en tarjetaService -> deleteCard(): ', error);
+		throw error;
+	}
+
+};
 
 
 
-function showLogsBannerGetAll() {
+function showLogsBanner(messageBannerLog) {
 	logger.info('=========================================');
-	logger.info('  Iniciando tarjetaService -> getAll()   ');
+	logger.info(messageBannerLog);
 	logger.info('=========================================');
 }
 
 
 module.exports = {
-    getAllService
+    getAllService,getIdService,createCardService,updateCardService,deleteCardService
 }
